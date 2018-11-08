e0ce47082384dcf636274871538e79822495b1ce
echoffee <echoffee@users.noreply.github.com>
2018-11-08 14:58:05 +0100

    Add database initialization + Fix connect.php

--- docker/custom-mysql/create_haisau_db.sql
+++ docker/custom-mysql/create_haisau_db.sql
@@ -1,1 +1,46 @@
-CREATE DATABASE haisu;
+CREATE DATABASE haisau;
+use haisau;
+CREATE TABLE Utilisateur (
+	idUtilisateur int NOT NULL AUTO_INCREMENT,
+	login varchar(255) NOT NULL,
+	password varchar(255) NOT NULL,
+	mail varchar(255),
+	PRIMARY KEY (idUtilisateur)
+);
+CREATE TABLE Projet (
+	idProjet int NOT NULL AUTO_INCREMENT,
+	nom varchar(255) NOT NULL,
+	dureeSprint int,
+	PRIMARY KEY(idProjet)
+);
+CREATE TABLE Travailler (
+	idTravailler int NOT NULL AUTO_INCREMENT,
+	idUtilisateur int NOT NULL,
+	idProjet int NOT NULL,
+	PRIMARY KEY(idTravailler)
+);
+CREATE TABLE Sprint (
+	idSprint int NOT NULL AUTO_INCREMENT,
+	nom varchar(255) NOT NULL,
+	dateDebut timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	dateFin timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	idProjet int NOT NULL,
+	PRIMARY KEY (idSprint)
+);
+CREATE TABLE UserStory (
+	idUserStory int NOT NULL AUTO_INCREMENT,
+	num int NOT NULL,
+	description text,
+	priorite varchar(16) NOT NULL,
+	difficulte int,
+	idProjet int NOT NULL,
+	PRIMARY KEY (idUserStory)
+);
+CREATE TABLE Tache (
+	idTache int NOT NULL AUTO_INCREMENT,
+	nom text NOT NULL,
+	cout decimal NOT NULL,
+	statut varchar(16) NOT NULL,
+	idProjet int NOT NULL,
+	PRIMARY KEY (idTache)
+);
--- src/connect.php
+++ src/connect.php
@@ -2,7 +2,7 @@
 <?php
 
 try {
-    $conn = new PDO('mysql:host=mysql;port=3306;dbname=haisu', 'root', 'root');
+    $conn = new PDO('mysql:host=mysql;port=3306;dbname=haisau', 'root', 'root');
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully \n";
