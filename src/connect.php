2c082f2f2abedbcc168ca5a435780600e094bd65
echoffee <echoffee@users.noreply.github.com>
2018-11-08 23:32:27 +0100

    Add createProject.php

--- src/connect.php
+++ src/connect.php
@@ -5,7 +5,6 @@ try {
     $conn = new PDO('mysql:host=mysql;port=3306;dbname=haisau', 'root', 'root');
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully \n";
     }
 catch(PDOException $e)
     {
---
+++ src/post_createProject.php
@@ -0,0 +1,16 @@
+<?php
+
+
+session_start();
+require 'connect.php';
+
+$projectName = $_POST['projectName'];
+$sprintDuration = $_POST['sprintDuration'];
+
+$query = "INSERT INTO Projet (nom, dureeSprint) VALUES ( '$projectName', $sprintDuration);";
+$result = $conn->exec($query) ;
+if ($result == 1)
+	echo "Project " . $projectName . " created successfully.";
+else
+	echo "An error occured while creating " . $projectName . ".";
+?>
