30ddde6c5c07ded5f2ba99961b8d0b1ef2632cc7
Haize Weiss <haize.weiss@etu.u-bordeaux.fr>
2018-11-09 01:05:10 +0100

    Update projectList.php

--- src/projectList.php
+++ src/projectList.php
@@ -24,6 +24,31 @@
           <th>Progress</th>
           <th>Current Sprint End Date</th>
         </tr>
+
+        <?php
+          // on se connecte à MySQL
+          $db = mysql_connect('localhost:8081', 'root', 'root');
+
+          // on sélectionne la base
+          mysql_select_db('haisau',$db);
+
+          // on crée la requête SQL
+          $sql = 'SELECT nom FROM Projet';
+
+          // on envoie la requête
+          $req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
+
+          // on fait une boucle qui va faire un tour pour chaque enregistrement
+          while($data = mysql_fetch_assoc($req))
+              {
+              // on affiche les informations de l'enregistrement en cours
+                echo'<tr><th>'.$data['nom'].'</th><th></th><th></th><th></th></tr>';
+              }
+
+          // on ferme la connexion à mysql
+          mysql_close();
+        ?> 
+
       </table> 
 
     <!-- Optional JavaScript -->
@@ -31,5 +56,6 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
   </body>
 </html>
--- src/userConnect.php
+++ src/userConnect.php
@@ -24,7 +24,6 @@ if (isset($_POST['login_user']))
         {
             $row = $stmt->fetch(); //fetch query parm into row array
             echo "l'utilisateur ".$row['login']. " est connecté avec le mail : ". $row['mail']."<br />";
-            header("location: projectList.php");
         }
 
 }
