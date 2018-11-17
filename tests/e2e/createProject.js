df69a33231b1107104877cff5b45cc30de5fd82a
echoffee <echoffee@users.noreply.github.com>
2018-11-17 17:31:33 +0100

    Update createProject.php test

--- tests/e2e/createProject.js
+++ tests/e2e/createProject.js
@@ -1,15 +1,15 @@
 module.exports = {
-    'Demo test' : function (client) {
+    'Create Project' : function (client) {
         client
             .url("http://webserver:80/createProject.php")
             .waitForElementVisible("body", 1000)
-            .setValue("input[name='projectName']", 'TestJS1')
+            .setValue("input[name='projectName']", 'DummyProjectName')
             .setValue("input[name='sprintDuration']", 42)
             .waitForElementVisible("input[id='bCreate']", 1000)
             .click("input[id='bCreate']")
             .pause(1000)
             .assert.title("Project List")
-            .assert.elementPresent("a", "TestJS1")
+            .assert.elementPresent("a", "DummyProjectName")
             .end();
     }
 };
