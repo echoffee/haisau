e1afb450c5fcb557c4fed2c30e790301e6a8c9d0
Haize Weiss <haize.weiss@gmail.com>
2018-11-18 22:52:59 +0100

    Update Delete Function

--- src/deleteProject.php
+++ src/deleteProject.php
@@ -38,5 +38,16 @@ else {
 	echo "An error occured while deleting the project";
 }
 
+$query = "DELETE FROM UserStory WHERE idProjet = $projectId;";
+
+$result = $conn->exec($query);
+var_dump($query);
+if ($result !== false) { 
+	echo "Project deleted successfully.";
+}
+else {
+	echo "An error occured while deleting the project";
+}
+
 header('Location: projectList.php');
 ?>
