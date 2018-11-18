77e74d3ca1c15d4b3b7327b48979c6d823f43ba3
echoffee <echoffee@users.noreply.github.com>
2018-11-18 01:08:35 +0100

    Update t5 tests

--- tests/e2e/createProject.js
+++ tests/e2e/createProject.js
@@ -10,6 +10,16 @@ module.exports = {
             .pause(1000)
             .assert.title("Project List")
             .assert.elementPresent("a", "DummyProjectName")
+            .url("http://webserver:80/createProject.php")
+            .waitForElementVisible("body", 1000)
+            .setValue("input[name='projectName']", "DummyProjectName")
+            .waitForElementVisible("input[id='bCreate']", 1000)
+            .click("input[id='bCreate']")
+            .pause(1000)
+            .assert.containsText("a", "Go back")
+            .click("a")
+            .pause(1000)
+            .assert.urlContains("createProject.php")
             .end();
     }
 };
