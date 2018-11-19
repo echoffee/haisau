c27e789e5c685b378bb3988193a56271c9fee9bf
echoffee <echoffee@users.noreply.github.com>
2018-11-19 15:05:46 +0100

    Update t5 & t6 tests + Add t1 tests
    CI tests are expected to fail until t1 specification is respected.

--- src/projectList.php
+++ src/projectList.php
@@ -57,7 +57,7 @@
               $projet['nbTachesTotal'] = "";
             }
 
-            echo "<tr><th><a href='userStoryList.php?id=". $projet['idProjet'] ."'>".$projet['nom']."</a><a href='modifyProject.php?id=".$projet['idProjet']."&name=".$projet['nom']."'><button type='button' id='edit-btn'>Edit</button></a><a href='deleteProject.php?id=".$projet['idProjet']."'><button type='button' id='delete-btn'>Delete</button></a></th><th>".$projet['currentSprint']['nom']."</th><th>".$projet['nbTachesDone']."/".$projet['nbTachesTotal']."</th><th>".$projet['currentSprint']['dateFin']."</th></tr>";
+            echo "<tr><th><a name='". $projet['nom']."' href='userStoryList.php?id=". $projet['idProjet'] ."'>".$projet['nom']."</a><a href='modifyProject.php?id=".$projet['idProjet']."&name=".$projet['nom']."'><button type='button' id='edit-btn'>Edit</button></a><a href='deleteProject.php?id=".$projet['idProjet']."'><button type='button' id='delete-btn'>Delete</button></a></th><th>".$projet['currentSprint']['nom']."</th><th>".$projet['nbTachesDone']."/".$projet['nbTachesTotal']."</th><th>".$projet['currentSprint']['dateFin']."</th></tr>";
 
           }
         ?> 
