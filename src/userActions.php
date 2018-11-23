bf8ce0d7f89184837895c4b5860e0f4446a33187
elbo <elboanas@gmail.com>
2018-11-23 15:59:33 +0100

    change logut button

--- src/userActions.php
+++ src/userActions.php
@@ -1,12 +1,4 @@
 <!-- Example single danger button -->
-<div class="btn-group">
-  <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-  <?php session_start(); echo "Welcome ".$_SESSION['login'] ?>
-  </button>
-  <div class="dropdown-menu">
-    <a class="dropdown-item" href="#">Invite developpers</a>
-    <a class="dropdown-item" href="#">recived invitations</a>
-    <div class="dropdown-divider"></div>
-    <a class="dropdown-item" href="userDisconnect.php">Logout</a>
-  </div>
+<div>
+<a href="userDisconnect.php " class="btn btn-danger btn-md">Logut</a>
 </div>
