37ef0e7e0d7ce067fd6e41db2773d844b9bcb22f
elbo <elboanas@gmail.com>
2018-12-14 00:13:32 +0100

    add porjects & userstory entries

--- docker/custom-mysql/create_haisau_db.sql
+++ docker/custom-mysql/create_haisau_db.sql
@@ -51,3 +51,35 @@ CREATE TABLE Tache (
 	PRIMARY KEY (idTache),
 	FOREIGN KEY (idSprint) REFERENCES Sprint(idSprint) ON DELETE CASCADE
 );
+
+/*add 5 project*/
+INSERT INTO `Projet` (`idProjet`, `nom`, `dureeSprint`) VALUES
+(NULL, 'Project1', '3'),
+(NULL, 'Project2', '3'), 
+(NULL, 'Project3', '2'), 
+(NULL, 'Project4', '2'),
+(NULL, 'Project5', '3');
+
+/*add 3 user story to each project*/
+INSERT INTO `UserStory` (`idUserStory`, `num`, `description`, `priorite`, `difficulte`, `type`, `idProjet`) VALUES
+
+/*project1*/
+(NULL, 1, 'This is us 1 ', 'Low', '10', 'backlog', '1'),
+(NULL, 2, 'This is us 2 ', 'Low', '10', 'backlog', '1'),
+(NULL, 3, 'This is us 3 ', 'Low', '10', 'sandbox', '1'),
+/*project2*/
+(NULL, 1, 'This is us 1 ', 'Low', '10', 'backlog', '2'),
+(NULL, 2, 'This is us 2 ', 'Low', '10', 'sandbox', '2'),
+(NULL, 3, 'This is us 3 ', 'Low', '10', 'backlog', '2'),
+/*project3*/
+(NULL, 1, 'This is us 1 ', 'Low', '10', 'backlog', '3'),
+(NULL, 2, 'This is us 2 ', 'Low', '10', 'sandbox', '3'),
+(NULL, 3, 'This is us 3 ', 'Low', '10', 'backlog', '3'),
+/*project4*/
+(NULL, 1, 'This is us 1 ', 'Low', '10', 'backlog', '4'),
+(NULL, 2, 'This is us 2 ', 'Low', '10', 'sandbox', '4'),
+(NULL, 3, 'This is us 3 ', 'Low', '10', 'backlog', '4'),
+/*project5*/
+(NULL, 1, 'This is us 1 ', 'Low', '10', 'backlog', '5'),
+(NULL, 2, 'This is us 2 ', 'Low', '10', 'sandbox', '5'),
+(NULL, 3, 'This is us 3 ', 'Low', '10', 'backlog', '5');
